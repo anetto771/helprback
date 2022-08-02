fa5fdbe0fdb4ea869c6d2281c6655a23cc180090
IkomaChan81 <victor.icoma@unesp.br>
2022-08-02 14:12:23 -0300

    Montagem do projeto inicial do back

--- helprback/src/main/java/com/api/helprback/HelprbackApplication.java
+++ helprback/src/main/java/com/api/helprback/HelprbackApplication.java
@@ -8,6 +8,6 @@ public class HelprbackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelprbackApplication.class, args);
+		System.out.println("API do Web App Helpr rodando!");
 	}
-
 }
