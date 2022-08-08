edba9e974d4e8b90d79e0c8e01f1e0461b5c3154
IkomaChan81 <victor.icoma@unesp.br>
2022-08-08 11:32:24 -0300

    Injeção de dados no banco de h2 testes.

---
+++ helprback/src/main/java/com/api/helprback/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.api.helprback.repositories;
+
+import com.api.helprback.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
