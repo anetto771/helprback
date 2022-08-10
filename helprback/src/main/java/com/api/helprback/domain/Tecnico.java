1b63c8f9a1f9190518cdcb6b1cd7d853298e600e
IkomaChan81 <victor.icoma@unesp.br>
2022-08-10 11:19:20 -0300

    Adição dos serviço e recurso de listagem por id em tecnico.

--- helprback/src/main/java/com/api/helprback/domain/Tecnico.java
+++ helprback/src/main/java/com/api/helprback/domain/Tecnico.java
@@ -1,5 +1,7 @@
 package com.api.helprback.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
@@ -8,6 +10,7 @@ import java.util.List;
 public class Tecnico extends Pessoa{
 
     private static final long serialVersionUID = 1L;
+    @JsonIgnore
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
---
+++ helprback/src/main/java/com/api/helprback/resources/TecnicoResource.java
@@ -0,0 +1,24 @@
+package com.api.helprback.resources;
+
+import com.api.helprback.domain.Tecnico;
+import com.api.helprback.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/service/tecnicos")
+public class TecnicoResource {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+        Tecnico obj = this.tecnicoService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
