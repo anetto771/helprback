3994f92d56688e5bae1b217b15d630e130887082
IkomaChan81 <victor.icoma@unesp.br>
2022-08-11 11:27:07 -0300

    Manipulando erro para entrega no front

--- helprback/src/main/java/com/api/helprback/resources/exceptions/ResourceExceptionHandler.java
+++ helprback/src/main/java/com/api/helprback/resources/exceptions/ResourceExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.api.helprback.resources.exceptions;
 
+import com.api.helprback.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
 public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError>
+    objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Object Not Found",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
