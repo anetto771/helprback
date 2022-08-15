1d46d136907f95832608feb8392def3514120637
IkomaChan81 <victor.icoma@unesp.br>
2022-08-15 10:00:25 -0300

    Tratamento da validação de erros por nulidade

--- helprback/src/main/java/com/api/helprback/resources/exceptions/ResourceExceptionHandler.java
+++ helprback/src/main/java/com/api/helprback/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.api.helprback.services.exceptions.DataIntegrityViolationException;
 import com.api.helprback.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -35,5 +37,20 @@ public class ResourceExceptionHandler {
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError>
+    validationErrors(MethodArgumentNotValidException ex,HttpServletRequest request){
+        ValidationError errors = new ValidationError(
+                System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation error",
+                "Erro na validação dos campos",
+                request.getRequestURI());
+        for (FieldError x : ex.getBindingResult().getFieldErrors()){
+            errors.addError(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
