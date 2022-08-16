9c046ebac94ac50bbc002774aaf222ed62c3ead4
Netto <anetto771@gmail.com>
2022-08-16 15:28:51 -0300

    Feito crud do Cliente no aplicativo HELPRBACK

--- helprback/src/main/java/com/api/helprback/domain/Cliente.java
+++ helprback/src/main/java/com/api/helprback/domain/Cliente.java
@@ -1,13 +1,21 @@
 package com.api.helprback.domain;
 
+import com.api.helprback.domain.dtos.ClienteDTO;
+import com.api.helprback.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 public class Cliente extends Pessoa{
 
     private static final long serialVersionUID = 1L;
+
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
@@ -18,6 +26,18 @@ public class Cliente extends Pessoa{
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
     }
+
+    public Cliente(ClienteDTO obj) {
+            super();
+            this.id = obj.getId();
+            this.nome = obj.getNome();
+            this.cpf = obj.getCpf();
+            this.email = obj.getEmail();
+            this.senha = obj.getSenha();
+            this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+            this.dataCriacao = obj.getDataCriacao();
+    }
+
 
     public List<Chamado> getChamados() {
         return chamados;
---
+++ helprback/src/main/java/com/api/helprback/domain/dtos/ClienteDTO.java
@@ -0,0 +1,110 @@
+package com.api.helprback.domain.dtos;
+
+import com.api.helprback.domain.Cliente;
+import com.api.helprback.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+
+    @NotNull(message = "O CAMPO NOME É REQUERIDO!")
+    protected String nome;
+
+    @NotNull(message = "O CAMPO CPF É REQUERIDO!")
+    protected String cpf;
+
+    @NotNull(message = "O CAMPO EMAIL É REQUERIDO!")
+    protected String email;
+
+    @NotNull(message = "O CAMPO SENHA É REQUERIDO!")
+    protected String senha;
+
+
+    protected Set<Integer> perfis = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public ClienteDTO(){
+        super();
+        addPerfis(Perfil.CLIENTE);
+    }
+
+    public ClienteDTO(Cliente obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+        addPerfis(Perfil.CLIENTE);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfis(Perfil perfis) {
+        this.perfis.add(perfis.getCodigo());
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+}
