7bcedfdc5faf87e117c9fc79f6261bfccf37b9b9
IkomaChan81 <victor.icoma@unesp.br>
2022-08-17 10:05:46 -0300

    CRUD de cliente exercício prático.

--- helprback/src/main/java/com/api/helprback/domain/Cliente.java
+++ helprback/src/main/java/com/api/helprback/domain/Cliente.java
@@ -1,9 +1,14 @@
 package com.api.helprback.domain;
 
+import com.api.helprback.domain.dtos.ClienteDTO;
+import com.api.helprback.domain.enums.Perfil;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 public class Cliente extends Pessoa{
 
@@ -15,6 +20,17 @@ public class Cliente extends Pessoa{
         super();
     }
 
+    public Cliente(ClienteDTO obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+        addPerfis(Perfil.CLIENTE);
+    }
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
     }
