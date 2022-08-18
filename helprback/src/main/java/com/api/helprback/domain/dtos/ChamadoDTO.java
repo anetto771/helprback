7dd5069449498b8ced4cc13145b5be5c2aeb1e44
IkomaChan81 <victor.icoma@unesp.br>
2022-08-18 11:01:55 -0300

    Adição de novos chamados com relacionamento conferido.

--- helprback/src/main/java/com/api/helprback/domain/dtos/ChamadoDTO.java
+++ helprback/src/main/java/com/api/helprback/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,7 @@ package com.api.helprback.domain.dtos;
 import com.api.helprback.domain.Chamado;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -14,11 +15,17 @@ public class ChamadoDTO implements Serializable {
     private LocalDate dataAbertura = LocalDate.now();
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+    @NotNull(message = "O campo PRIORIDADE é requerido.")
     private Integer prioridade;
+    @NotNull(message = "O campo STATUS é requerido.")
     private Integer status;
+    @NotNull(message = "O campo TÍTULO é requerido.")
     private String titulo;
+    @NotNull(message = "O campo OBSERVAÇÕES é requerido.")
     private String observacoes;
+    @NotNull(message = "O campo TÉCNICO é requerido.")
     private Integer tecnico;
+    @NotNull(message = "O campo CLIENTE é requerido.")
     private Integer cliente;
     private String nomeTecnico;
     private String nomeCliente;
--- helprback/src/main/java/com/api/helprback/services/ChamadoService.java
+++ helprback/src/main/java/com/api/helprback/services/ChamadoService.java
@@ -1,6 +1,11 @@
 package com.api.helprback.services;
 
 import com.api.helprback.domain.Chamado;
+import com.api.helprback.domain.Cliente;
+import com.api.helprback.domain.Tecnico;
+import com.api.helprback.domain.dtos.ChamadoDTO;
+import com.api.helprback.domain.enums.Prioridade;
+import com.api.helprback.domain.enums.Status;
 import com.api.helprback.repositories.ChamadoRepository;
 import com.api.helprback.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +18,11 @@ import java.util.Optional;
 public class ChamadoService {
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
@@ -21,5 +31,28 @@ public class ChamadoService {
 
     public List<Chamado> findAll() {
         return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO objDto) {
+        objDto.setId(null);
+        return repository.save(newChamado(objDto));
+    }
+
+    private Chamado newChamado(ChamadoDTO obj){
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+
+        return chamado;
     }
 }
