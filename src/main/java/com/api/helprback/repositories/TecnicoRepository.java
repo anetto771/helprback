ea022889bbeabc5e440cfbe4b8df85564d631fb1
IkomaChan81 <victor.icoma@unesp.br>
2022-08-30 15:33:54 -0300

    Adição do Relatório de Técnicos com chmados urgentes.

--- src/main/java/com/api/helprback/repositories/TecnicoRepository.java
+++ src/main/java/com/api/helprback/repositories/TecnicoRepository.java
@@ -1,7 +1,14 @@
 package com.api.helprback.repositories;
 
+import com.api.helprback.domain.Chamado;
 import com.api.helprback.domain.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+    @Query(value="CALL sps_relatorio_prioridade_alta_dia_atual(CURRENT_DATE())", nativeQuery=true)
+    Optional<List<Tecnico>> reportByTecnicoChamadoPrioridadeDia();
 }
